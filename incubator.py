cf767a473c446f012bbdf7693d37c45ef9822521
Derek Morris <ironchief89@gmail.com>
2014-05-29 14:41:17 -0700

    Feature: Add ability to read temp and log to gdocs

--- incubator.py
+++ incubator.py
@@ -1,8 +1,9 @@
-import time, string, urllib2, subprocess, re
+import time, string, urllib2, subprocess, re, gspread, datetime, environment
 import RPi.GPIO as io
 
 from time import strftime
 from lcdproc.server import Server
+from random import uniform
 io.setmode(io.BCM)
 
 def main():
@@ -20,16 +21,26 @@ def main():
     screen1.set_duration(5)
 
     # lcd strings
-    door_status = "shut"
-    temperature = "37"+chr(223)+"C"
-    humidity = "80% RH"
-    network = "wifi"
+    error = "log err"
+
+    # Account details for google docs
+    email       = environment.email
+    password    = environment.password
+    spreadsheet = environment.spreadsheet
+
+    try:
+        # Login with your Google account
+        gc = gspread.login(email, password)
+        # Open a worksheet from your spreadsheet using the filename
+        worksheet = gc.open(spreadsheet).sheet1
+    except:
+        network_status = error
 
     # lcd widgets
-    display_temperature = screen1.add_string_widget("MyTempWidget", text=temperature, x=1, y=1)
-    display_humidity = screen1.add_string_widget("MyHumiWidget", text=humidity, x=1, y=2)
-    display_network = screen1.add_string_widget("MyNetworkWidget", text=network, x=10, y=1)
-    display_alarm = screen1.add_string_widget("MyStringWidget", text=door_status, x=13, y=2)
+    display_temperature = screen1.add_string_widget("MyTempWidget", x=1, y=1)
+    display_humidity = screen1.add_string_widget("MyHumiWidget", x=1, y=2)
+    display_network = screen1.add_string_widget("MyNetworkWidget", x=10, y=1)
+    display_alarm = screen1.add_string_widget("MyStringWidget", x=13, y=2)
 
     while True:
         # get door status
@@ -40,22 +51,36 @@ def main():
 
         # get temperature and humidity status
         # output = subprocess.check_output(["./Adafruit_DHT", "2302", str(temp_pin)]);
-        # matches_temp = re.search("Temp =\s+([0-9.]+)", output)
-        # matches_hum = re.search("Hum =\s+([0-9.]+)", output)
-        # if matches_temp and matches_hum:
-        #     temperature = float(matches_temp.group(1))
-        #     humidity = float(matches_hum.group(1))
-        #     display_temperature.set_text(str(temp)+chr(223)+"C")
-        #     display_humidity.set_text(str(humidity)+"% RH")
-        # Append the data in the spreadsheet, including a timestamp
-        # get network status
-
-        if internet_on():
-            network = "   wifi"
+        temp = uniform( 36, 37)
+        hum = uniform( 75, 80)
+        output = "Temp = %.1f *C, Hum = %.1f %%" % (temp, hum)
+        matches_temp = re.search("Temp =\s+([0-9.]+)", output)
+        matches_hum = re.search("Hum =\s+([0-9.]+)", output)
+        if matches_temp and matches_hum:
+            temperature = float(matches_temp.group(1))
+            humidity = float(matches_hum.group(1))
+            temperature_status = str(temperature)+chr(223)+"C"
+            humidity_status = str(humidity)+"% RH"
         else:
-            network = "no wifi"
-      
-        display_network.set_text(network)
+            temperature_status = "--.-C"
+            humidity_status = "--.-% RH"
+
+
+        # get network status
+        if internet_on():
+            network_status = "   wifi"
+            if matches_temp and matches_hum:
+                try:
+                    values = [datetime.datetime.now(), temperature, humidity]
+                    worksheet.append_row(values)
+                except:
+                    network_status = error
+        else:
+            network_status = "no wifi"
+        
+        display_temperature.set_text(temperature_status)
+        display_humidity.set_text(humidity_status)
+        display_network.set_text(network_status)
         display_alarm.set_text(door_status)
         time.sleep(3)
 
